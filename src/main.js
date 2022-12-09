62a4ea4618213dfd1e0199913c6fe28cb4246417
asher <zhijian090195@hotmail.com>
2022-12-09 15:38:47 +0800

    [Added] 增加amplify套件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
+import AmplifyVue from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+import '@fontsource/inter/variable.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(AmplifyVue);
+app.mount('#app');
